7cae31079975ecb076fcbacfea7a0f5cabeaea4c
justinma98 <justinma3@gmail.com>
2018-11-21 13:05:28 -0800

    Updates

---
+++ precache-manifest.283c458e40639dcb396a84d594e07c73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/site/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5571d5242120e713b47d",
+    "url": "/site/static/js/runtime~main.5571d524.js"
+  },
+  {
+    "revision": "0e0024ad6ed61f50d30d",
+    "url": "/site/static/js/main.0e0024ad.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/site/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "0e0024ad6ed61f50d30d",
+    "url": "/site/static/css/main.01d80365.chunk.css"
+  },
+  {
+    "revision": "1f7b2d6cb45e1865d410febd75766d01",
+    "url": "/site/index.html"
+  }
+];
