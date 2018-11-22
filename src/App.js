18c8ffbcdf5a7e6c70b1156e7af0e9fabfdb7d6d
justinma98 <justinma3@gmail.com>
2018-11-21 23:23:18 -0800

    major changes

--- src/App.js
+++ src/App.js
@@ -12,9 +12,6 @@ class App extends Component {
   render() {
     return (
       <div class="dotcursor">
-        <div className="App-logo">
-          <h1>Welcome to Hell</h1>
-        </div>
         <div class = "Canvas">
         <Canvas id="myCanvas" ></Canvas>
         </div>
--- src/canvas.js
+++ src/canvas.js
@@ -1,14 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import P5Wrapper from "react-p5-wrapper";
 
 var lines = [];       // all lines
-var line_count = [];  // stores number of lines for every stroke
-let temp_line = [];		// stores last 'undo' line
-var lc = 0;           // line count for current stroke
-var sc = 0;           // stroke count
 var currentEvent, prevEvent;
 var movement_x, movement_y, movement = 5; // base stroke width is 5
-var mu = false;       //mouse up
+var drawing = false;
+var changing = false;
+var portal_x, portal_y;
+var temp_portal_x, temp_portal_y;
+var pc1, pc2, pc3;
+var bc1, bc2, bc3;
+var size = 45;
 
 document.documentElement.onmousemove=function(event){
   currentEvent = event;
@@ -25,125 +27,145 @@ setInterval(function(){
   prevEvent = currentEvent;
 },5);
 
+function translate_point( p, m ){
+  return p + Math.pow(p - m,3)/1000;
+}
+
+function rand_color() {
+  bc1 = pc1;
+  bc2 = pc2;
+  bc3 = pc3;
+  var temp;
+  switch (Math.floor(Math.random() * 3)){
+    case 0:
+      temp = pc1;
+      pc1 = pc2;
+      pc2 = temp;
+      break;
+    case 1:
+      temp = pc1;
+      pc1 = pc3;
+      pc3 = temp;
+      break;
+    case 2:
+      temp = pc2;
+      pc2 = pc3;
+      pc3 = temp;
+      break;
+    default:
+      break;
+  }
+  if (pc1 !== 0 && pc1 !== 255){
+    pc1 = Math.floor(Math.random() * 253) + 1;
+  }
+  if (pc2 !== 0 && pc2 !== 255){
+    pc2 = Math.floor(Math.random() * 253) + 1;
+  }
+  if (pc3 !== 0 && pc3 !== 255){
+    pc3 = Math.floor(Math.random() * 253) + 1;
+  }
+}
+
 const sketch = p5 => {
-  let curr_color;
-
-  const color_options = {
-    scheme_1: ["#94EBD8", "#00B349"],
-    scheme_2: ["#983275", "#FF6F01"],
-    scheme_3: ["#C3A706", "#329290"]
-  };
-
   p5.setup = () => {
     p5.createCanvas(p5.windowWidth, p5.windowHeight);
-    curr_color = p5.color("#000000");
-    p5.noFill();
+    p5.stroke("#000000");
+    portal_x = p5.windowWidth/2 - 25;
+    portal_y = p5.windowHeight/2 - 25;
+    pc1 = 100;
+    pc2 = 0;
+    pc3 = 255;
+    bc1 = 255;
+    bc2 = 100;
+    bc3 = 100;
   };
 
 	p5.draw = () => {
         p5.clear();
-        //p5.line(p5.mouseX,p5.mouseY,
-        //        p5.mouseX + 30 + p5.mouseX/10,
-        //        p5.mouseY - 30 - p5.mouseY/10);
-        p5.ellipse(p5.mouseX, p5.mouseY, movement * 5, movement * 5);
-        if (p5.mouseX != 0 || p5.mouseX != p5.pmouseX || p5.mouseY != p5.pmouseY){      
-          lines.push(new Line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY, movement,curr_color));
+        if (p5.mouseX > portal_x && p5.mouseX < portal_x + 50 &&
+            p5.mouseY > portal_y && p5.mouseY < portal_y + 50 && !changing){
+          drawing = true;
+          changing = true;
+          temp_portal_x = portal_x;
+          temp_portal_y = portal_y;
+          size = 50;
         }
-				for (var i = 0; i < lines.length; i++) {
-					p5.strokeWeight(lines[i].weight);
-		      p5.stroke(lines[i].color);
+        
+        //background
+        p5.strokeWeight(0);
+        console.log(.8/(Math.abs(portal_x - p5.mouseX) + 0.1));
+        p5.fill(bc1, bc2, bc3, 50000/(Math.sqrt(Math.pow(portal_x - p5.mouseX,2) + 
+                                                Math.pow(portal_y - p5.mouseY,2)) + 1));
+        p5.rect(0, 0, p5.windowWidth, p5.windowWidth);
 
-					// if the mouse is close to a point of a line, the point will 
-					// move be drawn close to the mouse
+        if (!drawing){
 
-		      p5.line(lines[i].x + Math.pow(lines[i].x - p5.mouseX,3)/1000, 
-		                lines[i].y + Math.pow(lines[i].y - p5.mouseY,3)/1000, 
-		               lines[i].px +  Math.pow(lines[i].px - p5.mouseX,3)/1000, 
-		                lines[i].py + Math.pow(lines[i].py - p5.mouseY,3)/1000);
-				  }
-			};
-	/*	
+          //first portal
+          p5.fill(pc1, pc2, pc3, [1]);
+          p5.rect(portal_x, portal_y, 50, 50);
 
-  p5.mousePressed = () => {
-		movement_x = 5;
-		movement_y = 5;
-		movement = 5;
-    mu = false;
-    lc = 0;
-  };
+        } else {
 
-  p5.mouseDragged = () => {
-    lines.push(new Line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY, 5,curr_color));
-    lc++;
-  };
+          // all strokes
+          if (p5.mouseX !== 0 || p5.mouseX !== p5.pmouseX || p5.mouseY !== p5.pmouseY){      
+            lines.push(new Line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY, movement));
+          }
+          for (var i = 0; i < lines.length; i++) {
+            p5.strokeWeight(lines[i].weight);
 
-  
-  p5.mouseReleased = () => {
-    line_count[sc] = lc;
-    sc++;
-    mu = true;
-  };
-*/
-  p5.keyPressed = () => {
-    switch (p5.key) {
-      case "R":
-      case "r":
-				lines = [];       // all lines
-        line_count = [];  // stores number of lines for every stroke
-				temp_line = [];		// stores last 'undo' line
-				lc = 0;           // line count for current stroke
-				sc = 0;           // stroke count
-				p5.clear();
-        curr_color = p5.color("#000000");
-        break;
-      case "T":
-        curr_color = p5.color(color_options.scheme_1[0]);
-        break;
-      case "G":
-        curr_color = p5.color(color_options.scheme_1[1]);
-        break;
-      case "Y":
-        curr_color = p5.color(color_options.scheme_2[0]);
-        break;
-      case "H":
-        curr_color = p5.color(color_options.scheme_2[1]);
-        break;
-      case "U":
-        curr_color = p5.color(color_options.scheme_3[0]);
-        break;
-      case "J":
-        curr_color = p5.color(color_options.scheme_3[1]);
-        break;
-       default:
-       	break;
-    }
-    
+            // if the mouse is close to a point of a line, the point will 
+            // move be drawn close to the mouse
 
-    if (p5.key === "D" || p5.key === "d") {
-      if (lines.length > 0 && mu){
-      	  //add deleted lines to deleted_lines array
-      	  temp_line = lines.slice(lines.length - line_count[sc-1], lines.length);	//store last stroke in temp array
-          lines.splice(lines.length - line_count[sc-1], line_count[sc-1]); //remove Drawing from array
-          sc--;
-          p5.clear();
-      }
-    }
+            p5.line(translate_point(lines[i].x, p5.mouseX), 
+                    translate_point(lines[i].y, p5.mouseY),
+                    translate_point(lines[i].px, p5.mouseX),
+                    translate_point(lines[i].py, p5.mouseY));
+          }
 
-    if (p5.key === "Q" || p5.key === "q") {
-    	if (mu){
-    		lines = lines.concat(temp_line);	//add line back to lines array
-    		sc++;
-    	}
-    }
-  };
+          //hidden portal
+          p5.strokeWeight(0);
+          p5.fill(pc1, pc2, pc3, [1]);
+          if (changing){
 
-  function Line(x, y, px, py, weight, color) {
+            if (temp_portal_x > 0){
+              temp_portal_x = Math.max(temp_portal_x - 45, 0);
+            } 
+            if (temp_portal_y > 0){
+              temp_portal_y = Math.max(temp_portal_y - 45, 0);
+            }
+            if (size < p5.windowWidth){
+            size+=100;
+            } else {
+              changing = false;
+              portal_x = Math.floor(Math.random() * (p5.windowWidth - 120) + 60);
+              portal_y = Math.floor(Math.random() * (p5.windowHeight - 120) + 60);
+              rand_color();
+              lines = [];
+            }
+            p5.rect(temp_portal_x,
+            temp_portal_y, 
+            size, 
+            size);
+          } else {
+            p5.rect(translate_point(portal_x, p5.mouseX),
+            translate_point(portal_y, p5.mouseY), 
+            45 + Math.pow(portal_x - p5.mouseX,2)/50, 
+            45 + Math.pow(portal_y - p5.mouseY,2)/50);
+          }
+        } 
+
+        //mouse
+        p5.noFill();
+        p5.strokeWeight(movement);
+        p5.ellipse(p5.mouseX, p5.mouseY, movement * 5, movement * 5);
+			};	
+
+  function Line(x, y, px, py, weight) {
     this.x = x;
     this.y = y;
     this.px = px;
     this.py = py;
     this.weight = weight;
-    this.color = color;
   }
 };
 
